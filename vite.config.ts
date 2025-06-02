da7de1105e50c0595edfcb6374a59fab51ff677e
IvanDancil <ivanbdancil@gmail.com>
2025-06-02 08:57:26 +0700

    Updated: latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const GITHUB_REPO_NAME = 'ReactPortfolio-v.2';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${GITHUB_REPO_NAME}/`,
 })
